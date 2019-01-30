a1237d259e8bc4fdacd5737b262cf580cb5139c2
visualstudio <visualstudio@122-PC24>
2019-01-30 11:27:06 -0500

    commit

--- Co730457/Program.cs
+++ Co730457/Program.cs
@@ -10,6 +10,43 @@ namespace Co730457
     {
         static void Main(string[] args)
         {
+
+        }
+    }
+    class Village
+    {
+        //Node is an ADT
+        // what KIND of DATA do we Need in a Node?
+        public Village nextVillage;
+        public Village previousVillage;
+        public string Villagename;
+        public bool isAstrildeHere = false;
+
+    }
+    class countryside
+    {
+        Village Maple = new Village();
+        Village Toronto = new Village();
+        Village Ajax = new Village();
+
+        Village First;
+        Village Last;
+
+        public void launch()
+        {
+            Maple.Villagename = "Maple";
+            Maple.nextVillage = Toronto;
+            Maple.previousVillage = null;
+            Toronto.Villagename = "Toronto:";
+            Toronto.nextVillage = Maple;
+            Toronto.previousVillage = Maple;
+            Ajax.Villagename = " Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage = Toronto;
+            
+
+
+
         }
     }
 }
